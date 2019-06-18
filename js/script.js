dc894a09a1a1d66cb6fc8c6f70e477f798b1c903
Ade Okeowo <adeo1234@hotmail.co.uk>
2019-06-18 13:07:41 +0100

    JS Update
    Updated js files from quiz

--- js/script.js
+++ js/script.js
@@ -19,39 +19,39 @@ $(document).ready(function() {
 
 function check() {
   var question1 = document.quiz.question1.value;
-  var question2 = document.quiz.question1.value;
-  var question3 = document.quiz.question1.value;
-  var question4 = document.quiz.question1.value;
-  var question5 = document.quiz.question1.value;
-  var question6 = document.quiz.question1.value;
-  var question7 = document.quiz.question1.value;
+  var question2 = document.quiz.question2.value;
+  var question3 = document.quiz.question3.value;
+  var question4 = document.quiz.question4.value;
+  var question5 = document.quiz.question5.value;
+  var question6 = document.quiz.question6.value;
+  var question7 = document.quiz.question7.value;
   var correct = 0;
 
-  if (question1 == "correctanswer") {
+  if (question1 == "Comedy") {
     correct++;
   }
-  if (question2 == "correctanswer") {
+  if (question2 == "Under 35") {
     correct++;
   }
-  if (question3 == "correctanswer") {
+  if (question3 == "750,000") {
     correct++;
   }
-  if (question4 == "correctanswer") {
+  if (question4 == "Tablet") {
     correct++;
   }
-  if (question5 == "correctanswer") {
+  if (question5 == "At home") {
     correct++;
   }
-  if (question6 == "correctanswer") {
+  if (question6 == "6") {
     correct++;
   }
-  if (question7 == "correctanswer") {
+  if (question7 == "Desert Island Discs") {
     correct++;
   }
 
-var messages = ["Great job!", "That's just okay", "You really need to do better"];
+var messages = ["You really need to do better", "Better luck next tim", "We cant all be winners", "Good try", "Keep going", "Almost there", "So close", "Great job!", "You are amazing!"];
 
-var pictures = ["images/clap.gif", "images/try.gif", "images/help.gif"];
+var pictures = ["images/clap.gif", "images/try.gif", "images/help.gif", "images/meh.jpeg", "images/next.gif", "images/lose.gif", "images/win.gif", "images/sparkles.gif", "images/Winner.gif"];
 
 var range;
 
@@ -68,7 +68,7 @@ var range;
   }
 
 document.getElementById("after_submit").style.visibility = "visible";
-document.getElementById("message").innerHTML = messages[range];
+document.getElementById("message").innerHTML = messages[correct];
 document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
-document.getElementById("picture").src = pictures[range];
+document.getElementById("picture").src = pictures[correct];
 }
